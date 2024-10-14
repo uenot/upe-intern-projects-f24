792b5cc55c90e40f79c4c8e844de8b367bd346be
Daniel Matuzka <dmatuzka@bu.edu>
2024-10-14 11:41:02 -0400

    Created recommendation algorithm for music based on mood using song features like energy, acousticness, loudness and others

--- Daniel-Matuzka-Project/client/src/App.js
+++ Daniel-Matuzka-Project/client/src/App.js
@@ -1,14 +1,28 @@
-import './App.css';
-import Home from './pages/Home'
+import React, { createContext, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import './App.css';
 
-function App() {
+// Create a MoodContext to hold the songs and update function
+export const MoodContext = createContext();
+
+const App = () => {
+  const [songs, setSongs] = useState([]);
+  const [currentMood, setCurrentMood] = useState('');
+
+  const queryClient = new QueryClient(); // Create a QueryClient instance
+
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <QueryClientProvider client={queryClient}> {/* Wrap with QueryClientProvider */}
+      <MoodContext.Provider value={{ songs, setSongs, currentMood, setCurrentMood }}> {/* Provide context */}
+        <div className="App">
+          <Navbar />
+          <Home />
+        </div>
+      </MoodContext.Provider>
+    </QueryClientProvider>
   );
-}
+};
 
 export default App;
